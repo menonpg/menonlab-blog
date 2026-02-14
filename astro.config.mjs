25924ab527299d175661fb7265711c994279ac85
Eric Wimsatt <ewimsatt@gmail.com>
2026-02-14 10:41:47 -0600

    Add syntax highlighting, RSS link in blog, 404 page, improved code block styling

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,10 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://omarcms.com',
+  markdown: {
+    shikiConfig: {
+      theme: 'github-dark',
+      wrap: true,
+    },
+  },
 });
